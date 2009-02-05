036ea7b8780ed4512d8e659e2e48cb901e9f8025
Jean-Luc Thiffeault <jeanluc@mailaps.org>
2009-02-05 21:08:22 +0000

    Added functions ReciprocalPolynomialBoundedList, PolynomialRoots, PerronRoot.

--- PseudoAnosov.m
+++ PseudoAnosov.m
@@ -13,6 +13,12 @@ ReciprocalPolynomial::usage = "ReciprocalPolynomial[x,n] returns a reciprocal po
 
 ReciprocalPolynomialCoefficientBounds::usage = "ReciprocalPolynomialCoefficientBounds[n,t] lists the bound on the magnitude of the coefficients {a[1],...,a[Floor[n/2]]} of a reciprocal polynomial x^n + a[1] x^(n-1) + a[2] x^(n-2) + ... + a[2] x^2 + a[1] x + 1, given that its largest eigenvalue is h with t = h + 1/h.";
 
+ReciprocalPolynomialBoundedList::usage = "ReciprocalPolynomialBoundedList[x,n,amax] returns a list of reciprocal polynomials x^n + a[1] x^(n-1) + a[2] x^(n-2) + ... + a[2] x^2 + a[1] x + 1 with coefficients bounded by |a[k]| <= amax[k].  For n even, only one of each polynomials pair P(-x)=P(x) is listed.";
+
+PolynomialRoots::usage = "PolynomialRoots[p,x] returns the roots of the polynomial p(x), sorted in decreasing order of magnitude.";
+
+PerronRoot::usage = "PerronRoot[p,x] returns the largest root (in magnitute) of the polynomial p(x).";
+
 
 Begin["`Private`"]
 
@@ -59,6 +65,53 @@ ReciprocalPolynomialCoefficientBounds[n_,t_] := Module[
        large as possible. *)
     Simplify[Table[(-1)^k c[[k+1]]/.hsub,{k,n/2}]]
 ]
+
+
+(* Very kludgy: eliminate 'by hand' the duplicate polynomials that are
+   related by P(X)=P(-X), since these have the same dominant
+   eigenvalue.  Only hand-coded for n=2,4,6,8 for now. *)
+(* To do: implement the elimination of P(-x) for any even n. *)
+ReciprocalPolynomialBoundedList[x_,n_,a_List] := Module[
+    {p,c,l,l2},
+    p = ReciprocalPolynomial[x,n,c];
+    If[n == 2 || n == 4 || (n > 8 && EvenQ[n]),
+        l = Flatten[Fold[
+	    Table[#1,{c[#2],-a[[#2]],a[[#2]]}]&, p, Table[k,{k,2,n/2}]
+        ]];
+        Flatten[Table[l,{c[1],-a[[1]],0}]]
+    ,
+    If[n == 6 || n == 8,
+        (* First list the cases with c[1]>0 *)
+        l = Table[p,{c[1],-a[[1]],-1}];
+        l = Flatten[Fold[
+	    Table[#1,{c[#2],-a[[#2]],a[[#2]]}]&, l, Table[k,{k,2,n/2}]
+        ]];
+        (* Then list the cases with c[1]=0, c[3]>=0 *)
+        l2 = Table[p/.c[1]->0,{c[3],-a[[3]],0},{c[2],-a[[2]],a[[2]]}];
+        l2 = Flatten[Fold[
+	    Table[#1,{c[#2],-a[[#2]],a[[#2]]}]&, l2, Table[k,{k,4,n/2}]]];
+	Join[l,l2]
+        ,
+        (* If all else fails, just include everything. *)
+	Flatten[Fold[
+	    Table[#1,{c[#2],-a[[#2]],a[[#2]]}]&, p, Table[k,{k,1,n/2}]
+        ]]
+    ]
+    ]
+]
+
+
+PolynomialRoots[p_,x_,opts:OptionsPattern[]] :=
+    Sort[x/.NSolve[p == 0, x, opts], Abs[#2] < Abs[#1] &]
+
+(* PolynomialRoots inherits the options for NSolve *)
+Options[PolynomialRoots] = Options[NSolve]
+
+
+PerronRoot[p_,x_,opts:OptionsPattern[]] := First[PolynomialRoots[p,x,opts]]
+
+(* PerronRoot inherits the options for NSolve *)
+Options[PerronRoot] = Options[NSolve]
 
 
 End[(* "`Private`" *)]
