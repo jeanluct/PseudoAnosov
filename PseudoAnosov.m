25a74617c2f074f2d9699a2bde0de51a89a191aa
Jean-Luc Thiffeault <jeanluc@mailaps.org>
2009-02-06 15:45:53 +0000

    * Added hand-coded ReciprocalPolynomialBoundedList[x,n,h] for n=6,8,    which allows saerching through the polynomials for a dilatation    less than h without making the list directly.  This confirms table    4 in the paper.  * Added MahlerMeasure, ReciprocalPolynomialQ, and MinimalPolynomialQ.

--- PseudoAnosov.m
+++ PseudoAnosov.m
@@ -3,6 +3,10 @@
 BeginPackage["PseudoAnosov`", {"Global`"}]
 
 
+(*
+   Command usage messages
+*)
+
 PseudoAnosov::usage = "Functions for manipulating characteristic polynomials of pseudo-Anosov maps."
 
 TracesPower::usage = "TracesPower[p,x,m], where p(x) is the characteristic polynomial of a matrix M, lists the traces Tr[M^k] for 1 <= k <= m."
@@ -11,6 +15,8 @@ LefschetzNumbers::usage = "LefschetzNumbers[p,x,m], where p(x) is the characteri
 
 ReciprocalPolynomial::usage = "ReciprocalPolynomial[x,n] returns a reciprocal polynomial x^n + a[1] x^(n-1) + a[2] x^(n-2) + ... + a[2] x^2 + a[1] x + 1.  ReciprocalPolynomial[x,n,c] uses c as the base name for coefficients.  ReciprocalPolynomial[x,n,{a_1,...,a_(n/2)}] uses a list for the coefficient, where (n/2) denotes Floor[n/2].";
 
+ReciprocalPolynomialQ::usage = "ReciprocalPolynomialQ[p,x] returns true if p(x) is a reciprocal polynomial, i.e. of the form a[0] x^n + a[1] x^(n-1) + a[2] x^(n-2) + ... + a[2] x^2 + a[1] x + a[0].";
+
 ReciprocalPolynomialCoefficientBounds::usage = "ReciprocalPolynomialCoefficientBounds[n,t] lists the bound on the magnitude of the coefficients {a[1],...,a[Floor[n/2]]} of a reciprocal polynomial x^n + a[1] x^(n-1) + a[2] x^(n-2) + ... + a[2] x^2 + a[1] x + 1, given that its largest eigenvalue is h with t = h + 1/h.";
 
 ReciprocalPolynomialBoundedList::usage = "ReciprocalPolynomialBoundedList[x,n,amax] returns a list of reciprocal polynomials x^n + a[1] x^(n-1) + a[2] x^(n-2) + ... + a[2] x^2 + a[1] x + 1 with coefficients bounded by |a[k]| <= amax[k].  For n even, only one of each polynomials pair P(-x)=P(x) is listed.";
@@ -18,6 +24,17 @@ ReciprocalPolynomialBoundedList::usage = "ReciprocalPolynomialBoundedList[x,n,am
 PolynomialRoots::usage = "PolynomialRoots[p,x] returns the roots of the polynomial p(x), sorted in decreasing order of magnitude.";
 
 PerronRoot::usage = "PerronRoot[p,x] returns the largest root (in magnitute) of the polynomial p(x).";
+
+MahlerMeasure::usage = "MahlerMeasure[p,x] returns the Mahler measure of the polynomial p(x), which is the absolute value of the product of roots outside the unit circle.";
+
+MinimalPolynomialQ::usage = "MinimalPolynomialQ[p] returns true if the polynomial p cannot be factored.";
+
+
+(*
+   Error messages and warnings
+*)
+
+PseudoAnosov::notminimal = "Not a minimal polynomial."
 
 
 Begin["`Private`"]
@@ -51,6 +68,13 @@ ReciprocalPolynomial[x_,n_,c_:Global`a] :=
     ReciprocalPolynomial[x,n,Table[c[k],{k,n/2}]]
 
 
+ReciprocalPolynomialQ[p_,x_] := Module[
+    {c = CoefficientList[Collect[p,x],x], n},
+    n = Length[c]-1;
+    Return[Simplify[p - x^n (p/.x->1/x)] === 0]
+]
+
+
 ReciprocalPolynomialCoefficientBounds[n_,t_] := Module[
     {poly,h,hh,c,ssol},
     (* Make a reciprocal polynomial with eigenvalues h and 1/h,
@@ -81,12 +105,12 @@ ReciprocalPolynomialBoundedList[x_,n_,a_List] := Module[
         Flatten[Table[l,{c[1],-a[[1]],0}]]
     ,
     If[n == 6 || n == 8,
-        (* First list the cases with c[1]>0 *)
+        (* First list the cases with c[1]<0 *)
         l = Table[p,{c[1],-a[[1]],-1}];
         l = Flatten[Fold[
 	    Table[#1,{c[#2],-a[[#2]],a[[#2]]}]&, l, Table[k,{k,2,n/2}]
         ]];
-        (* Then list the cases with c[1]=0, c[3]>=0 *)
+        (* Then list the cases with c[1]=0, c[3]<=0 *)
         l2 = Table[p/.c[1]->0,{c[3],-a[[3]],0},{c[2],-a[[2]],a[[2]]}];
         l2 = Flatten[Fold[
 	    Table[#1,{c[#2],-a[[#2]],a[[#2]]}]&, l2, Table[k,{k,4,n/2}]]];
@@ -94,10 +118,50 @@ ReciprocalPolynomialBoundedList[x_,n_,a_List] := Module[
         ,
         (* If all else fails, just include everything. *)
 	Flatten[Fold[
-	    Table[#1,{c[#2],-a[[#2]],a[[#2]]}]&, p, Table[k,{k,1,n/2}]
+	    Table[#1,{c[#2],-a[[#2]],a[[#2]]}]&, p, Table[k,{k,n/2}]
         ]]
     ]
     ]
+]
+
+(* These versions of the function takes a dilatation as an argument
+   rather than a coefficient list.  They are hand-coded for specific
+   n, which is ugly but this function is only needed for specific,
+   difficult cases. *)
+ReciprocalPolynomialBoundedList[x_,6,h_Real] := Module[
+    {p,n = 6,c,a,pl = {},hh,hmin = 1+10^-4, degen = 10^-5},
+    a = Floor/@ReciprocalPolynomialCoefficientBounds[n,h+1/h];
+    p = ReciprocalPolynomial[x,n,c];
+    keep[hh_] := ((hmin < hh[[1]] < h) && Abs[hh[[1]]-hh[[2]]] > degen);
+    Do[
+        hh = Abs/@Take[PolynomialRoots[p,x],2];
+        If[keep[hh], pl=Append[pl,p]; Print[p]]
+    ,{c[1],-a[[1]],-1},{c[2],-a[[2]],a[[2]]},{c[3],-a[[3]],a[[3]]}];
+    p = p/.{c[1]->0};
+    Do[
+        hh = Abs/@Take[PolynomialRoots[p,x],2];
+        If[keep[hh], pl=Append[pl,p]; Print[p]]
+    ,{c[2],-a[[2]],a[[2]]},{c[3],-a[[3]],0}];
+    pl
+]
+ReciprocalPolynomialBoundedList[x_,8,h_Real] := Module[
+    {p,n = 8,c,a,pl = {},hh,hmin = 1+10^-4, degen = 10^-5, cases = 0},
+    a = Floor/@ReciprocalPolynomialCoefficientBounds[n,h+1/h];
+    p = ReciprocalPolynomial[x,n,c];
+    keep[hh_] := ((hmin < hh[[1]] < h) && Abs[hh[[1]]-hh[[2]]] > degen);
+    Do[
+        If[Mod[++cases,10000] == 0, Print[cases]];
+        hh = Abs/@Take[PolynomialRoots[p,x],2];
+        If[keep[hh], pl=Append[pl,p]; Print[p]]
+    ,{c[1],-a[[1]],-1},{c[2],-a[[2]],a[[2]]}
+    ,{c[3],-a[[3]],a[[3]]},{c[4],-a[[4]],a[[4]]}];
+    p = p/.{c[1]->0};
+    Do[
+        If[Mod[++cases,10000] == 0, Print[cases]];
+        hh = Abs/@Take[PolynomialRoots[p,x],2];
+        If[keep[hh], pl=Append[pl,p]; Print[p]]
+    ,{c[2],-a[[2]],a[[2]]},{c[3],-a[[3]],0},{c[4],-a[[4]],a[[4]]}];
+    pl
 ]
 
 
@@ -112,6 +176,17 @@ PerronRoot[p_,x_,opts:OptionsPattern[]] := First[PolynomialRoots[p,x,opts]]
 
 (* PerronRoot inherits the options for NSolve *)
 Options[PerronRoot] = Options[NSolve]
+
+
+MahlerMeasure[p_,x_,opts:OptionsPattern[]] := Module[{},
+    If[!MinimalPolynomialQ[p],Message[PseudoAnosov::notminimal]];
+    Fold[Times,1,Select[Abs/@PolynomialRoots[p,x,opts],#>1&]]]
+
+(* MahlerMeasure inherits the options for NSolve *)
+Options[MahlerMeasure] = Options[NSolve]
+
+
+MinimalPolynomialQ[p_] := Factor[p] === Expand[p]
 
 
 End[(* "`Private`" *)]
