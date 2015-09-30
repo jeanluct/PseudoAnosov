245f2deeabe1c0d71c1a4d7a023b301bc890fcb6
Jean-Luc Thiffeault <jeanluc@mailaps.org>
2015-09-30 10:36:59 -0500

    A few edits when arranging functions for the README.md file.

--- PseudoAnosov.m
+++ PseudoAnosov.m
@@ -14,33 +14,33 @@ PseudoAnosov::usage = "Functions for manipulating characteristic polynomials of 
 
 PolynomialDegree::usage = "PolynomialDegree[P] returns the degree of the polynomial P(x)."
 
-PolynomialRoots::usage = "PolynomialRoots[P] returns the roots of the polynomial P, sorted in decreasing order of magnitude.";
+PolynomialRoots::usage = "PolynomialRoots[P] returns the roots of the polynomial P, sorted in decreasing order of magnitude."
 
-PerronRoot::usage = "PerronRoot[P] returns the largest root (in magnitude) of the polynomial P.";
+PerronRoot::usage = "PerronRoot[P] returns the largest root (in magnitude) of the polynomial P."
 
-PseudoAnosovPerronRootQ::usage = "PseudoAnosovPerronRootQ[P] returns True if the largest root of the polynomial P is nondegenerate (in magnitude) and real.  PseudoAnosovPerronRootQ[P,xmax] also returns False unless the the Perron root is less than xmax (in magnitude).";
+PseudoAnosovPerronRootQ::usage = "PseudoAnosovPerronRootQ[P] returns True if the largest root of the polynomial P is nondegenerate (in magnitude) and real.  PseudoAnosovPerronRootQ[P,xmax] also returns False unless the the Perron root is less than xmax (in magnitude)."
 
-MahlerMeasure::usage = "MahlerMeasure[P] returns the Mahler measure of the polynomial P, which is the absolute value of the product of roots outside the unit circle.";
+MahlerMeasure::usage = "MahlerMeasure[P] returns the Mahler measure of the polynomial P, which is the absolute value of the product of roots outside the unit circle."
 
 TracesPower::usage = "TracesPower[P,k], where P is the characteristic polynomial of some matrix M, returns the trace Tr[M^k].  TracesPower[P,{k2}] returns a list of traces Tr[M^k] for 1 <= k <= k2.  TracesPower[P,{k1,k2}] returns a list of traces Tr[M^k] for k1 <= k <= k2."
 
-ReciprocalPolynomial::usage = "ReciprocalPolynomial[x,n] returns a reciprocal polynomial x^n + a[1] x^(n-1) + a[2] x^(n-2) + ... + a[2] x^2 + a[1] x + 1.  ReciprocalPolynomial[x,n,c] uses c as the base name for coefficients.  ReciprocalPolynomial[x,n,{a_1,...,a_(n/2)}] uses a list for the coefficient, where (n/2) denotes Floor[n/2].";
+ReciprocalPolynomial::usage = "ReciprocalPolynomial[x,n] returns a reciprocal polynomial x^n + a[1] x^(n-1) + a[2] x^(n-2) + ... + a[2] x^2 + a[1] x + 1.  ReciprocalPolynomial[x,n,c] uses c as the base name for coefficients.  ReciprocalPolynomial[x,n,{a_1,...,a_(n/2)}] uses a list for the coefficient, where (n/2) denotes Floor[n/2]."
 
-ReciprocalPolynomialQ::usage = "ReciprocalPolynomialQ[P] returns true if P is a reciprocal polynomial, i.e. of the form a[0] x^n + a[1] x^(n-1) + a[2] x^(n-2) + ... + a[2] x^2 + a[1] x + a[0].";
+ReciprocalPolynomialQ::usage = "ReciprocalPolynomialQ[P] returns True if P is a reciprocal polynomial, i.e. of the form a[0] x^n + a[1] x^(n-1) + a[2] x^(n-2) + ... + a[2] x^2 + a[1] x + a[0]."
 
 PolynomialFromTraces::usage = "PolynomialFromTraces[x,T,det] creates a polynomial of degree Length[T]+1 from the determinant det (defaults to 1) and a list T of traces of powers of its associated matrix."
 
 ReciprocalPolynomialFromTraces::usage = "ReciprocalPolynomialFromTraces[x,n,T] creates a reciprocal polynomial of degree n from a list of traces of powers of its associated matrix.  ReciprocalPolynomialFromTraces[x,T] creates a polynomial of degree 2 Length[T]."
 
-PolynomialBoundedList::usage = "PolynomialBoundedList[x,n,r,a[n]] returns a list of polynomials x^n + a[1] x^(n-1) + a[2] x^(n-2) + ... + a[n-2] x^2 + a[n-1] x + a[n] with Perron root less than r.  For n even, only one of each polynomial pair P(-x)=P(x) is listed.  If not specified, a[n] (determinant) defaults to 1.";
+PolynomialBoundedList::usage = "PolynomialBoundedList[x,n,r,a[n]] returns a list of polynomials x^n + a[1] x^(n-1) + a[2] x^(n-2) + ... + a[n-2] x^2 + a[n-1] x + a[n] with Perron root less than r.  For n even, only one of each polynomial pair P(-x)=P(x) is listed.  If not specified, a[n] (determinant) defaults to 1."
 
-ReciprocalPolynomialBoundedList::usage = "ReciprocalPolynomialBoundedList[x,n,r] returns a list of reciprocal polynomials x^n + a[1] x^(n-1) + a[2] x^(n-2) + ... + a[2] x^2 + a[1] x + 1 with Perron root less than r.  For n even, only one of each polynomial pair P(-x)=P(x) is listed.";
+ReciprocalPolynomialBoundedList::usage = "ReciprocalPolynomialBoundedList[x,n,r] returns a list of reciprocal polynomials x^n + a[1] x^(n-1) + a[2] x^(n-2) + ... + a[2] x^2 + a[1] x + 1 with Perron root less than r.  For n even, only one of each polynomial pair P(-x)=P(x) is listed."
 
-IrreducibleMatrixQ::usage = "IrreducibleMatrixQ[M] returns true if the matrix M is irreducible.";
+IrreducibleMatrixQ::usage = "IrreducibleMatrixQ[M] returns True if the matrix M is irreducible."
 
-OrientableStrataList::usage = "OrientableStrataList[g] gives the list of orientable strata for a hyperbolic surface of genus g.  Each stratum in the list is of the form {k_1,...,k_m}, where k_i is the (even) degree of each singularity, and the sum over the k_i gives -2(Euler Characteristic).  Use Tally/@OrientableStrataList[g] to group singularities by multiplicity.";
+OrientableStrataList::usage = "OrientableStrataList[g] gives the list of orientable strata for a hyperbolic surface of genus g.  Each stratum in the list is of the form {k_1,...,k_m}, where k_i is the (even) degree of each singularity, and the sum over the k_i gives -2(Euler Characteristic).  Use Tally/@OrientableStrataList[g] to group singularities by multiplicity."
 
-StrataList::usage = "StrataList[g] gives the list of orientable strata for a hyperbolic surface of genus g.  Each stratum in the list is of the form {k_1,...,k_m}, where k_i is the degree of each singularity, and the sum over the k_i gives -2(Euler Characteristic).  Use Tally/@StrataList[g] to group singularities by multiplicity.  (Punctures not yet implemented.)";
+StrataList::usage = "StrataList[g] gives the list of orientable strata for a hyperbolic surface of genus g.  Each stratum in the list is of the form {k_1,...,k_m}, where k_i is the degree of each singularity, and the sum over the k_i gives -2(Euler Characteristic).  Use Tally/@StrataList[g] to group singularities by multiplicity.  (Punctures not yet implemented.)"
 
 StratumDoubleCover::usage := "StratumDoubleCover[S] gives the stratum corresponding to the orientating double-cover of the stratum S={k_1,...,k_m}."
 
@@ -52,7 +52,7 @@ LefschetzNumbers::usage = "LefschetzNumbers[P,k], where P is the characteristic 
 
 LefschetzCombine::usage = "LefschetzCombine[L1,L2,...] adds lists of Lefschetz number.  If they are not the same length, then the blocks are repeated to the length of the longest list.\nLefschetzCombine[L1,L2,...,Lm,n] caps the total length at an integer n."
 
-LefschetzNumbersTestQ::usage = "LefschetzNumbersTestQ[S,P] returns true if the polynomial P is compatible with the stratum S.  Possible options are GiveReasonForRejection (default False), MaxIterate (default 1), and MaxLefschetz (default 50)."
+LefschetzNumbersTestQ::usage = "LefschetzNumbersTestQ[S,P] returns True if the polynomial P is compatible with the stratum S.  Possible options are GiveReasonForRejection (default False), MaxIterate (default 1), and MaxLefschetz (default 50)."
 
 StratumOrbits::usage = "StratumOrbits[S,P] returns a list of possible orbit structure (singular and regular periodic orbits) for the polynomial P on stratum S.  Returns an empty list if this proves impossible.\nStratumOrbits[S,L] does the same for a list of Lefschetz numbers L."
 
@@ -768,7 +768,7 @@ SingularStratum[s_List, prs_Integer:-1] := Module[
 ]
 
 
-Regular::usage = "PseudoAnosov`Lefschetz`Orbits`Regular[L], where L is a list of Lefschetz numbers, returns the list of regular periodic orbits compatible with L, unless an incompatible orbit is detected, in which cases the function stops and returns what it's found.  The sign of the Perron root can be specified by the option PerronRootSign (default Automatic)."
+Regular::usage = "PseudoAnosov`Lefschetz`Orbits`Regular[L], where L is a list of Lefschetz numbers, returns the list of regular periodic orbits compatible with L, unless an incompatible orbit is detected, in which cases the function stops and returns what it found.  The sign of the Perron root can be specified by the option PerronRootSign (default Automatic)."
 
 Regular[L_List, OptionsPattern[]] := Module[
     {rpo, def, prs = OptionValue[PerronRootSign]},
